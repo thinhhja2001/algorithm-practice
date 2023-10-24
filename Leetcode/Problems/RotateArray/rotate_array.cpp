849846f29a04713dc75aa1a832b33060a03026e8
thinhhja2001 <thinhnguyendoan5122001@gmail.com>
2023-10-24 15:17:03 +0700

    rotate array

---
+++ Leetcode/Problems/RotateArray/rotate_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+void rotate(vector<int> &nums, int k)
+{
+    int size = nums.size();
+    int actualRotate = k % size;
+    vector<int> rotated(size, 0);
+    for (int i = 0; i < size; i++)
+    {
+        int index;
+        if (i + actualRotate >= size)
+        {
+            index = i - size + actualRotate;
+        }
+        else
+        {
+            index = i + actualRotate;
+        }
+        if (rotated[index] != 0)
+            continue;
+        rotated[index] = nums[i];
+    }
+    nums = rotated;
+}
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    rotate(nums, k);
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+}
