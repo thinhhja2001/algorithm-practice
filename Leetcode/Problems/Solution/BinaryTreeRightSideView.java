71b1df19359045b45956fd354b6e57359a4b2d9e
thinhhja2001 <thinhnguyendoan5122001@gmail.com>
2025-09-06 15:39:29 +0700

    binary tree right side view

--- Leetcode/Problems/Solution/BinaryTreeRightSideView.java
+++ Leetcode/Problems/Solution/BinaryTreeRightSideView.java
@@ -26,19 +26,22 @@ public class BinaryTreeRightSideView {
   }
 
   public List<Integer> rightSideView(TreeNode root) {
-    if(root==null){
-      return  new ArrayList<>();
+    if (root == null) {
+      return new ArrayList<>();
     }
-    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> result = new ArrayList<>();
     Queue<TreeNode> queue = new LinkedList<>();
     queue.offer(root);
-    int level = 0;
     while (!queue.isEmpty()) {
       int len = queue.size();
-      result.add(new ArrayList<>());
       for (int i = 0; i < len; i++) {
         TreeNode node = queue.poll();
-        result.get(level).add(node.val);
+
+        if (i == len - 1) {
+          // end of current queue
+          result.add(node.val);
+        }
+
         if (node.left != null) {
           queue.offer(node.left);
         }
@@ -46,20 +49,19 @@ public class BinaryTreeRightSideView {
           queue.offer(node.right);
         }
       }
-      level++;
     }
-    return result.stream().map(List::getLast).toList();
+    return result;
 
   }
 
   void main() {
     TreeNode root = new TreeNode(1);
     root.left = new TreeNode(2);
-    root.right=new TreeNode(3);
+    root.right = new TreeNode(3);
     root.left.left = new TreeNode(4);
     root.left.left.left = new TreeNode(5);
 
-    List<Integer>result = rightSideView(root);
+    List<Integer> result = rightSideView(root);
     System.out.println(result);
   }
 }
