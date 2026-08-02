2e0f322d16670591d12804cc9e3573cb0b4619cd
thinh.nguyen <thinh.nguyen@doctoranywhere.com>
2026-08-02 12:24:33 +0700

    Weekly 513

--- Leetcode/Problems/Solution/Weekly513/CountOfUnfinishedTasksAfterEachShift.java
+++ Leetcode/Problems/Solution/Weekly513/CountOfUnfinishedTasksAfterEachShift.java
@@ -7,20 +7,18 @@ public class CountOfUnfinishedTasksAfterEachShift {
 
     int[] result = new int[shifts.length];
 
-    int[] prefixSum = new int[tasks.length];
+    long[] prefixSum = new long[tasks.length];
 
     prefixSum[0] = tasks[0];
     for (int i = 1; i < tasks.length; i++) {
       prefixSum[i] = tasks[i] + prefixSum[i - 1];
     }
 
-    int remember = 0;
+    long remember = 0;
 
     int current = 0;
     for (int i = 0; i < shifts.length; i++) {
-      while (current < prefixSum.length && shifts[i] >= prefixSum[current] - remember) {
-        current++;
-      }
+      current = lowerBound(prefixSum, shifts[i], remember, current);
 
       if (current >= prefixSum.length) {
         result[i] = 0;
@@ -36,12 +34,29 @@ public class CountOfUnfinishedTasksAfterEachShift {
     return result;
   }
 
+  public static int lowerBound(long[] array, long shift, long remember, int current) {
+    int low = current, high = array.length;
+    int mid;
+    while (low < high) {
+      mid = low + (high - low) / 2;
+      if (shift >= array[mid] - remember) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+
+    if (low < array.length && shift > array[low] - remember) {
+      low++;
+    }
+    return low;
+  }
+
   public static void main(String[] args) {
     int[] tasks = {1, 1, 3, 3, 8};
     int[] shifts = {2, 9, 5, 3, 9};
+
     System.out.println(Arrays.toString(countTasks(tasks, shifts)));
-
-
   }
 }
 
